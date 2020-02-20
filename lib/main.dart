6172c168b99826f0fa424dd29a78eb3f80082a28
Rody Davis <rody.davis.jr@gmail.com>
2020-02-19 20:50:17 -0800

    updating mac signing

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SFXP Meetup',
       routes: {
         '/': (BuildContext context) {
