5a0145f15509abf21706230154792aa412e15e6e
Rody Davis <rody.davis.jr@gmail.com>
2020-02-19 21:14:14 -0800

    add code editing

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sfxp_meetup/widget/fold_listview.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -25,11 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               child: Column(
-                children: [
-                  header(),
-                  SizedBox(height: 25),
-                  card()
-                ],
+                children: [header(), SizedBox(height: 25), card()],
               ),
             ),
             Padding(
@@ -42,6 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  @widget
   Widget header() {
     return Row(
       children: [
@@ -53,28 +51,35 @@ class _HomeScreenState extends State<HomeScreen> {
             color: Color(0xFFDDDDDD),
             borderRadius: BorderRadius.circular(25),
             boxShadow: [
-              BoxShadow(offset: Offset(5, 5), color: Color.fromARGB(40, 0, 0, 0), blurRadius: 8),
-              BoxShadow(offset: Offset(-5, -5), color: Color.fromARGB(150, 255, 255, 255), blurRadius: 8)
+              BoxShadow(
+                  offset: Offset(5, 5),
+                  color: Color.fromARGB(40, 0, 0, 0),
+                  blurRadius: 8),
+              BoxShadow(
+                  offset: Offset(-5, -5),
+                  color: Color.fromARGB(150, 255, 255, 255),
+                  blurRadius: 8)
             ],
           ),
         ),
         Expanded(
             child: Center(
-              child: Text(
-                'Cinema Plus',
-                style: TextStyle(
-                  fontFamily: "Product",
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 30,
-                ),
-              ),
-            )),
+          child: Text(
+            'Cinema Plus',
+            style: TextStyle(
+              fontFamily: "Product",
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              fontSize: 30,
+            ),
+          ),
+        )),
         SizedBox(width: 50)
       ],
     );
   }
 
+  @widget
   Widget card() {
     final size = MediaQuery.of(context).size;
     double percentageX = (localX / (size.width - 40)) * 100;
@@ -93,7 +98,11 @@ class _HomeScreenState extends State<HomeScreen> {
           color: Color(0xFFCCCCCC),
           borderRadius: BorderRadius.circular(15),
           boxShadow: [
-            BoxShadow(offset: Offset(0, 60), color: Color.fromARGB(120, 0, 0, 0), blurRadius: 22, spreadRadius: -20),
+            BoxShadow(
+                offset: Offset(0, 60),
+                color: Color.fromARGB(120, 0, 0, 0),
+                blurRadius: 22,
+                spreadRadius: -20),
           ],
         ),
         child: GestureDetector(
@@ -107,8 +116,10 @@ class _HomeScreenState extends State<HomeScreen> {
           onPanUpdate: (details) {
             setState(() {
               setState(() => defaultPosition = false);
-              if (details.localPosition.dx > 0 && details.localPosition.dy < 230) {
-                if (details.localPosition.dx < size.width - 40 && details.localPosition.dy > 0) {
+              if (details.localPosition.dx > 0 &&
+                  details.localPosition.dy < 230) {
+                if (details.localPosition.dx < size.width - 40 &&
+                    details.localPosition.dy > 0) {
                   localX = details.localPosition.dx;
                   localY = details.localPosition.dy;
                 }
@@ -124,8 +135,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   Transform(
                     transform: Matrix4.identity()
-                      ..translate(defaultPosition ? 0.0 : (8 * (percentageX / 50) + -8),
-                          defaultPosition ? 0.0 : (8 * (percentageY / 50) + -8), 0.0),
+                      ..translate(
+                          defaultPosition ? 0.0 : (8 * (percentageX / 50) + -8),
+                          defaultPosition ? 0.0 : (8 * (percentageY / 50) + -8),
+                          0.0),
                     alignment: FractionalOffset.center,
                     child: Opacity(
                       opacity: 0.4,
@@ -164,8 +177,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   Transform(
                     transform: Matrix4.identity()
-                      ..translate(defaultPosition ? 0.0 : (15 * (percentageX / 50) + -15),
-                          defaultPosition ? 0.0 : (15 * (percentageY / 50) + -15), 0.0),
+                      ..translate(
+                          defaultPosition
+                              ? 0.0
+                              : (15 * (percentageX / 50) + -15),
+                          defaultPosition
+                              ? 0.0
+                              : (15 * (percentageY / 50) + -15),
+                          0.0),
                     alignment: FractionalOffset.center,
                     child: Padding(
                       padding: const EdgeInsets.only(right: 22),
@@ -178,7 +197,8 @@ class _HomeScreenState extends State<HomeScreen> {
                               padding: const EdgeInsets.only(left: 22, top: 15),
                               child: Padding(
                                 padding: const EdgeInsets.only(top: 5),
-                                child: Image.asset("assets/images/sfxp.png", width: 90, color: Colors.white),
+                                child: Image.asset("assets/images/sfxp.png",
+                                    width: 90, color: Colors.white),
                               ),
                             ),
                           ),
@@ -206,7 +226,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               Text(
                                 'THRU 09/22',
                                 style: TextStyle(
-                                    fontFamily: "CreditCard", color: Colors.white.withOpacity(0.8), fontSize: 12),
+                                    fontFamily: "CreditCard",
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontSize: 12),
                               ),
                             ],
                           ),
